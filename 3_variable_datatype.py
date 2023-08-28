61b58bd5356633e17d19f46adb6fce2f6f71556f
kelvintan1101 <kelvintan1101@gmail.com>
2023-08-28 18:27:44 +0800

    First push

---
+++ 3_variable_datatype.py
@@ -0,0 +1,9 @@
+character_name = "Kelvin"
+character_age = 24
+is_male = True
+if(is_male):
+    print("There once was a man named " + character_name + ", ")
+    print("he was " + str(character_age) + " years old. ")
+    character_name = "Kelvin Tan"
+    print("He really liked the name " + character_name + ", ")
+    print("but didn't like being " + str(character_age) + ".")
---
+++ 4_string.py
@@ -0,0 +1,6 @@
+phrase = "KelvinTan"
+phrase_1 = "Kelvin\nTan"
+phrase_2 = "Kelvin\"Tan"
+print(phrase + " is cool")
+print(phrase_1)
+print(phrase_2)
